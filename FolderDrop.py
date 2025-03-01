a4f575993691acaec720638e200f137e2b64db36
Marius <privacyfirst555@proton.me>
2025-03-01 11:21:23 +0200

    Added a '-d' and '--directory' option that is saved to app.config as 'directory'

--- FolderDrop.py
+++ FolderDrop.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+import argparse
 
 app = Flask(__name__)
 
@@ -9,4 +10,8 @@ def index():
 
 # run the application
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Python script for sharing a directory over the internet.")
+    parser.add_argument('-d', '--directory',  default='./share', help='Specify the share directory. Default "./share"')
+    args = parser.parse_args()
+    app.config['directory'] = args.directory
     app.run(debug=True)
