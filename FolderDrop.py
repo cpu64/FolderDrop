b734f358a0b15297828b7eb116aa47f1815a863a
Dovydas Jonuška <dovydas.jonuska345@gmail.com>
2025-03-02 18:34:43 +0200

    Added a login screen

--- FolderDrop.py
+++ FolderDrop.py
@@ -1,11 +1,29 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, session, redirect, url_for, request
 import argparse
+import os
 
 app = Flask(__name__)
 
+password = "test".encode('utf-8')
+
+# Secret key for session management (important for keeping track of user sessions)
+app.secret_key = os.urandom(24) # Can be a string for testing purposes but should be random in production
+# app.secret_key = "abc123" # For testing purposes
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if password == request.form["password"].encode('utf-8'):
+            session['logged_in'] = True  # Mark the user as logged in
+            return redirect(url_for('index'))  # Redirect to the home page
+        else:
+            return render_template('login.html', error="Incorrect password, try again.")
+    return render_template('login.html')
+
 @app.route("/")
 def index():
+    if password and not session.get('logged_in'):
+        return redirect(url_for('login'))  # Redirect to the login page if not logged in
     return render_template('index.html')
 
 # run the application
